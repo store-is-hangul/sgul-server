b0c61c5bf94f7929d8588acf5ba7e20a6cc4c126
JackCokebb <reo2726@gmail.com>
2025-10-13 22:25:45 +0900

    init

---
+++ src/test/java/org/storeishangul/sgulserver/SgulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.storeishangul.sgulserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgulServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
