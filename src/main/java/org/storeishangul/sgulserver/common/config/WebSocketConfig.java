af05cc19662cba30856afc76b9bbe59c75e93643
JackCokebb <reo2726@gmail.com>
2025-11-10 23:30:59 +0900

    feature: websocket heart beat 설정 추가

--- src/main/java/org/storeishangul/sgulserver/common/config/WebSocketConfig.java
+++ src/main/java/org/storeishangul/sgulserver/common/config/WebSocketConfig.java
@@ -1,7 +1,9 @@
 package org.storeishangul.sgulserver.common.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -14,7 +16,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
 
         // 클라이언트가 구독할 prefix
-        registry.enableSimpleBroker("/topic", "/queue");
+        registry.enableSimpleBroker("/topic", "/queue")
+            .setHeartbeatValue(new long[]{10000, 10000})
+                .setTaskScheduler(heartBeatScheduler());
 
         // 클라이언트가 메시지 보낼 prefix (클라이언트가 @MessageMapping이 붙은 컨트롤러 메서드를 호출할 때 사용)
         registry.setApplicationDestinationPrefixes("/app");
@@ -29,5 +33,16 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws-sgul")
             .setAllowedOriginPatterns("*")
             .withSockJS();
+    }
+
+    // 하트비트 스케줄러
+    @Bean
+    public ThreadPoolTaskScheduler heartBeatScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(1);
+        scheduler.setThreadNamePrefix("stomp-heartbeat-thread-");
+        scheduler.initialize();
+
+        return scheduler;
     }
 }
