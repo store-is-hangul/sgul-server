4ee00e3118343e8037ec57123097d2ab6865debc
JackCokebb <reo2726@gmail.com>
2025-11-18 00:02:41 +0900

    feature: websocket 연결 문제 수정

--- src/main/java/org/storeishangul/sgulserver/common/exception/handler/WebSocketExceptionHandler.java
+++ src/main/java/org/storeishangul/sgulserver/common/exception/handler/WebSocketExceptionHandler.java
@@ -3,7 +3,6 @@ package org.storeishangul.sgulserver.common.exception.handler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.socket.messaging.StompSubProtocolErrorHandler;
@@ -17,6 +16,7 @@ public class WebSocketExceptionHandler extends StompSubProtocolErrorHandler {
 
     // 특정 예외 처리
     @MessageExceptionHandler(CustomException.class)
+    @SendToUser("/queue/errors")
     public WebsocketExceptionResponse handleIllegalArgument(CustomException e) {
 
         log.error("Custom Exception: {}", e.getMessage(), e);
@@ -24,6 +24,7 @@ public class WebSocketExceptionHandler extends StompSubProtocolErrorHandler {
     }
 
     @MessageExceptionHandler(RuntimeException.class)
+    @SendToUser("/queue/errors")
     public WebsocketExceptionResponse handleIllegalArgument(RuntimeException e) {
 
         log.error("Internal Server Error: {}", e.getMessage(), e);
