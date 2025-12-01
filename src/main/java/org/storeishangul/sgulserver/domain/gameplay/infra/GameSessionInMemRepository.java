eac965a0d76eb2ca6430181c681124b2bf378018
JackCokebb <reo2726@gmail.com>
2025-12-01 22:25:18 +0900

    feature: 우리말 API 인터페이스 정의

--- src/main/java/org/storeishangul/sgulserver/domain/gameplay/infra/GameSessionInMemRepository.java
+++ src/main/java/org/storeishangul/sgulserver/domain/gameplay/infra/GameSessionInMemRepository.java
@@ -1,33 +1,45 @@
 package org.storeishangul.sgulserver.domain.gameplay.infra;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.stereotype.Repository;
 import org.storeishangul.sgulserver.domain.gameplay.domain.model.GameSession;
 
 @Repository
 public class GameSessionInMemRepository implements GameSessionRepository{
 
+    private final Map<String, GameSession> gameSessions;
+
+    public GameSessionInMemRepository() {
+
+        this.gameSessions = new HashMap<>();
+    }
+
     @Override
     public GameSession findByUserId(String userId) {
-        //TODO
 
-        return null;
+        return gameSessions.get(userId);
     }
 
     @Override
     public GameSession save(GameSession gameSession) {
-        //TODO
 
-        return null;
+        gameSessions.put(gameSession.getUserId(), gameSession);
+
+        return gameSession;
     }
 
     @Override
     public GameSession update(GameSession gameSession) {
-        //TODO
-        return null;
+
+        gameSessions.put(gameSession.getUserId(), gameSession);
+
+        return gameSession;
     }
 
     @Override
     public void deleteByUserId(String userId) {
-        //TODO
+
+        gameSessions.remove(userId);
     }
 }
--- src/main/java/org/storeishangul/sgulserver/domain/gameplay/infra/GameSessionRepository.java
+++ src/main/java/org/storeishangul/sgulserver/domain/gameplay/infra/GameSessionRepository.java
@@ -1,9 +1,11 @@
 package org.storeishangul.sgulserver.domain.gameplay.infra;
 
+import jakarta.annotation.Nullable;
 import org.storeishangul.sgulserver.domain.gameplay.domain.model.GameSession;
 
 public interface GameSessionRepository {
 
+    @Nullable
     GameSession findByUserId(String userId);
 
     GameSession save(GameSession gameSession);
